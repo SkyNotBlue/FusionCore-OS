db91f62e9ededdcb2afd7e4f31b7233b16bad3eb
BagelMakesDev <bagelmakesdev@gmail.com>
2023-02-26 20:16:25 -0800

    Adds the VS2022 files.

---
+++ FusionCoreX86/FusionCoreX86/CoreConsole.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+namespace FusionCoreX86
+{
+    internal class CoreConsole
+    {
+        internal void FusionCoreConsole()
+        {
+            Console.Write("Ready. ");
+            var input = Console.ReadLine();
+
+            if (input.StartsWith("echo "))
+            {
+                EchoCommand(input);
+            }
+            else if (input.StartsWith("theme "))
+            {
+                ThemeCommand(input);
+            }
+            else
+            {
+                Console.WriteLine("Invalid command.");
+            }
+
+
+        }
+
+        private static void EchoCommand(string input)
+        {
+            string echoText = input.Substring(5);
+            Console.WriteLine(echoText);
+            }
+        private static void ThemeCommand(string input)
+        {
+                string ThemeText = input.Substring(6);
+            if (ThemeText == "Hacker")
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+                else _ = (ThemeText == "Darkmode");
+                {
+                    public ConsoleColor Background { get; set; }
+
+
+                }
+        }
+    }
+}
---
+++ FusionCoreX86/FusionCoreX86/Kernel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+namespace FusionCoreX86
+{
+    public class FusionCore : Sys.Kernel
+    {
+
+        protected override void BeforeRun()
+        {
+            Console.WriteLine("FusionCore has booted successfully.");
+            Console.WriteLine("FusionCoreX86 Version b0.0.1 Codename Stanley");
+        }
+
+        protected override void Run()
+        {
+            CoreConsole ConsInit = new CoreConsole();
+            ConsInit.FusionCoreConsole();
+        }
+                
+    }
+
+}
+
